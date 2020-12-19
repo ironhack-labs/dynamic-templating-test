5345c844b6ea196b013db99ca205f235e24ffe18
Christian <christian.garba@gmail.com>
2020-12-19 09:56:23 -0700

    done

--- server.js
+++ server.js
@@ -1,23 +1,31 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const hbs = require('hbs');
+const express = require("express");
+const hbs = require("hbs");
+const students = require("./exam-info");
 
 const app = express();
 
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/views");
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // 1: in the home,list all the students who took the exam (list all the students)
 
-// ... Your code here
+app.get("/", (request, response) => {
+  response.render("full-list.hbs", { students });
+});
 
 // 2: in the '/results' list all the students who passed the test and their score.
 // Also, students should be in descending order based on their score.
 
-// ... Your code here
+app.get("/results", (request, response) => {
+  const filteredStudents = students
+    .filter((student) => student.hasPassed)
+    .sort((a, b) => b.score - a.score);
+  response.render("results.hbs", { filteredStudents });
+});
 
 app.listen(process.env.PORT, () =>
   console.log(`App running on ${process.env.PORT}.`)
